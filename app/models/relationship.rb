8ab1d59dbf3a0b107d0a88d5a99628ca8da7dd1f
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-12-06 18:17:00 +0000

    [update]relationship association

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,13 @@
 class Relationship < ApplicationRecord
+  # follower (フォローする人)
+  # foreign_keyは省略可能だが、可読性を高めるために明示。
+  # class_name: 'User' で外部キーがUserモデルを参照することを明示
+  belongs_to :follower, class_name: "User"
+  
+  # followed (フォローされる人)
+  belongs_to :followed, class_name: "User"
+  
+  # バリデーション
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,18 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
+  # follower (フォローする人)
+  # foreign_keyは省略可能だが、可読性を高めるために明示。
+  # class_name: 'User' で外部キーがUserモデルを参照することを明示
+  belongs_to :follower, class_name: "User"
+  
+  # followed (フォローされる人)
+  belongs_to :followed, class_name: "User"
+  
+  # バリデーション
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
